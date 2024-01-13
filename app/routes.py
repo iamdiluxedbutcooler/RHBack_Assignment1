9da160e8d33ab9862b2c7bcec79c51be548a9d30
iamdiluxedbutcooler <iamdiluxedbutcooler@gmail.com>
2024-01-13 16:29:49 +0700

    Assignment 2 update
    to mongo db

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,23 @@
-from app import app
-from flask import jsonify
-
-@app.route('/')
-def home():
-    return "Hello Backend"
+from app import app, mongo
+from flask import jsonify, request, Response
 
 @app.route('/about_me',
-           methods =  ['POST'])
+           methods =  ['POST', 'GET'])
 def about_me():
-    return jsonify({"Name": "Pleng Litchiowong", 
-                    "Course": "Computer Science", 
-                    "Year": "1", 
-                    "CCAs": "RHDevs, RHMP, BlkComm, WelfareComm, OCIP, RVC SN, BOP, AnG"})
+    if request.method == 'POST':
+        data = request.get_json()
+        mongo.db.users.insert_one(data)
+        return Response("Entry added", status = 201)
+    elif request.method == 'GET':
+        name = request.args.get('name')
+        user = mongo.db.users.find_one({"Name": name})
+        if user:
+            user.pop('_id')
+            return jsonify(user)
+        else:
+            return Response("User not found", status = 404)
+    
+    #return jsonify({"Name": "Pleng Litchiowong", 
+    #                "Course": "Computer Science", 
+    #                "Year": "1", 
+    #                "CCAs": "RHDevs, RHMP, BlkComm, WelfareComm, OCIP, RVC SN, BOP, AnG"})
